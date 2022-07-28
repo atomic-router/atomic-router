3dbfadc1cb626c6622ca82a14888019cccfa5428
Anton Kosykh <kelin2025@yandex.ru>
2022-07-28 17:25:17 +0300

    test: Added test cases for `redirect`

---
+++ test/redirect.test.ts
@@ -0,0 +1,115 @@
+import { createEvent, createStore } from 'effector';
+import { createRoute, redirect } from '../src';
+
+const sleep = (t: number) => {
+  return new Promise((r) => {
+    setTimeout(r, t);
+  });
+};
+
+describe('redirect', () => {
+  it('Opens `route` on `clock` trigger', async () => {
+    const clock = createEvent();
+    const route = createRoute();
+
+    redirect({
+      clock,
+      route,
+    });
+
+    clock();
+
+    await sleep(0);
+
+    expect(route.$isOpened.getState()).toBeTruthy();
+    expect(route.$params.getState()).toEqual({});
+    expect(route.$query.getState()).toEqual({});
+  });
+
+  // TODO: Would be cool to make it default behavior
+  // However, it'll be a breaking change
+  // For now it's just { params: {}, query: {} } if `params/query` is empty
+  // it('Takes `params` & `query` directly from `clock`', async () => {
+  //   const clock = createEvent<{
+  //     params: { foo: string };
+  //     query: { baz: string };
+  //   }>();
+  //   const route = createRoute<{ foo: string }>();
+
+  //   redirect({
+  //     clock,
+  //     route,
+  //   });
+
+  //   clock({
+  //     params: { foo: 'bar' },
+  //     query: { baz: 'test' },
+  //   });
+
+  //   await sleep(0);
+
+  //   expect(route.$isOpened.getState()).toBeTruthy();
+  //   expect(route.$params.getState()).toEqual({ foo: 'bar' });
+  //   expect(route.$query.getState()).toEqual({ baz: 'test' });
+  // });
+
+  it('Object-like `params` & `query`', async () => {
+    const clock = createEvent();
+    const route = createRoute<{ foo: string }>();
+
+    redirect({
+      clock,
+      params: { foo: 'bar' },
+      query: { baz: 'test' },
+      route,
+    });
+
+    clock();
+
+    await sleep(0);
+
+    expect(route.$isOpened.getState()).toBeTruthy();
+    expect(route.$params.getState()).toEqual({ foo: 'bar' });
+    expect(route.$query.getState()).toEqual({ baz: 'test' });
+  });
+
+  it('Store-like `params` & `query`', async () => {
+    const clock = createEvent();
+    const route = createRoute<{ foo: string }>();
+
+    redirect({
+      clock,
+      params: createStore({ foo: 'bar' }),
+      query: createStore({ baz: 'test' }),
+      route,
+    });
+
+    clock();
+
+    await sleep(0);
+
+    expect(route.$isOpened.getState()).toBeTruthy();
+    expect(route.$params.getState()).toEqual({ foo: 'bar' });
+    expect(route.$query.getState()).toEqual({ baz: 'test' });
+  });
+
+  it('Function-like `params` & `query`', async () => {
+    const clock = createEvent<string>();
+    const route = createRoute<{ foo: string }>();
+
+    redirect({
+      clock,
+      params: (foo) => ({ foo }),
+      query: (foo) => ({ baz: `${foo}-test` }),
+      route,
+    });
+
+    clock('bar');
+
+    await sleep(0);
+
+    expect(route.$isOpened.getState()).toBeTruthy();
+    expect(route.$params.getState()).toEqual({ foo: 'bar' });
+    expect(route.$query.getState()).toEqual({ baz: 'bar-test' });
+  });
+});
