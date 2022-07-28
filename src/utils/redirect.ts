5648cae330995a24443a7ed20381ac3274e1be44
Anton Kosykh <kelin2025@yandex.ru>
2022-07-28 17:25:07 +0300

    ts: Improved `redirect` typings

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -9,18 +9,35 @@ import {
 } from 'effector';
 import { RouteInstance, RouteQuery } from '../types';
 
-type RedirectParams<T, Params> = {
-  clock?: Clock<T>;
-  route: RouteInstance<Params>;
-  params?: ((clock: T) => Params) | Store<Params> | Params;
-  query?: ((clock: T) => RouteQuery) | Store<RouteQuery> | RouteQuery;
-};
+type RedirectParams<T, Params> = Params extends { [key in string]: never }
+  ? {
+      clock?: Clock<T>;
+      route: RouteInstance<Params>;
+      query?: ((clock: T) => RouteQuery) | Store<RouteQuery> | RouteQuery;
+    }
+  :
+      | {
+          clock?: Clock<T>;
+          route: RouteInstance<Params>;
+          params: ((clock: T) => Params) | Store<Params> | Params;
+          query?: ((clock: T) => RouteQuery) | Store<RouteQuery> | RouteQuery;
+        }
+      | {
+          clock?: Clock<{
+            params: Params;
+            query?: RouteQuery;
+          }>;
+          route: RouteInstance<Params>;
+          params?: ((clock: T) => Params) | Store<Params> | Params;
+          query?: ((clock: T) => RouteQuery) | Store<RouteQuery> | RouteQuery;
+        };
 
 /** Opens passed `route` upon `clock` trigger */
 export const redirect = <T, Params>(options: RedirectParams<T, Params>) => {
   const clock = options.clock
     ? sample({ clock: options.clock as Event<T> })
     : createEvent<T>();
+  // @ts-expect-error
   let params = toStore(options.params || {});
   let query = toStore(options.query || {});
 
