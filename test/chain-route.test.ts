0ae430a14cb9db1339439690cfc687b3fa9cacb9
Kirill Mironov <vetrokm@gmail.com>
2022-08-03 14:18:30 +0300

    style: fix formatting

--- test/chain-route.test.ts
+++ test/chain-route.test.ts
@@ -25,11 +25,9 @@ describe('chainRoute', () => {
 
   it('attach-like config in beforeOpen', () => {
     const route = createRoute<{ x: string }>();
-    const cb = vi.fn(
-      (payload: { param: string; queryParam: string }) => {
-        return payload;
-      }
-    );
+    const cb = vi.fn((payload: { param: string; queryParam: string }) => {
+      return payload;
+    });
     const fx = createEffect(cb);
     const chainedRoute = chainRoute({
       route,
